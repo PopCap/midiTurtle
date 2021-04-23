f923f8d6b3ff43d8390059d35d97da5981fa15a5
cantrehl <59850691+cantrehl@users.noreply.github.com>
2021-04-23 15:14:22 -0400

    HandleExport first functionality
    yeet

--- MusicDrawerApplication.java
+++ MusicDrawerApplication.java
@@ -139,7 +139,33 @@ public abstract class MusicDrawerApplication extends JApplication implements Act
    */
   protected void handleExport()
   {
+    try
+    {
+      Component contentPane = getGUIComponent();
+      BufferedImage image = new BufferedImage(contentPane.getWidth(), contentPane.getHeight(),
+          BufferedImage.TYPE_INT_RGB);
 
+      BufferedImage img = image.getSubimage(0, 0, contentPane.getWidth(), contentPane.getHeight());
+
+      Graphics2D g2d = img.createGraphics();
+      g2d = img.createGraphics();
+      contentPane.printAll(g2d);
+      g2d.dispose();
+      try
+      {
+        Random random = new Random();
+        ImageIO.write(img, "png",
+            new File(
+                getClass().getProtectionDomain().getCodeSource().getLocation().toURI().getPath()
+                    + "\\shapeDrawerApplicationImage_" + random.nextInt(100) + ".png"));
+      } catch (URISyntaxException e)
+      {
+        e.printStackTrace();
+      }
+    } catch (IOException ex)
+    {
+      ex.printStackTrace();
+    }
   }
 
   /**
